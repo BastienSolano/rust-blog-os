713031ef9afaebbb471ac3002edf7278d34892e6
Bastien Solano <bastien.solano@gmail.com>
2023-02-27 00:28:22 +0100

    Writing lines

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello world";
-
 // We need to implement a function that will be called by rust upon panicking
 // Note: this function should never return , so the return type is '!'
 //       it is a diverging funciton
