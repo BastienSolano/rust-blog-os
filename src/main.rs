63aedbdd5875e367600e59403906f07f627ddc14
Bastien Solano <bastien.solano@gmail.com>
2023-02-26 22:58:27 +0100

    Hello world & cargo runner ocnfig

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello world";
+
 // We need to implement a function that will be called by rust upon panicking
 // Note: this function should never return , so the return type is '!'
 //       it is a diverging funciton
@@ -14,5 +16,15 @@ fn panic(_info: &PanicInfo) -> ! {
 // Since we have no underlying operating system, we need to provide a starting point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // We need an unsafe block because rust cannot be sure that our raw pointer is correct
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // character byte
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // color byte (0xb is light cyan)
+        }
+    }
+
     loop {}
 }
